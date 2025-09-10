30bb2d0481ccce39a9bf951f18e5d50a579e1fde
Jaiyash Godani <techieboy1212@gmail.com>
2025-09-10 22:56:25 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan all js/jsx/ts/tsx files in src
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
